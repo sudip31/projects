e8ac329753017aa09bed7c37e82e707383da08a6
Sudipta Jana <95089569+sudip31@users.noreply.github.com>
2023-01-06 21:44:22 +0530

    Update logic.js

--- LoginPage/logic.js
+++ LoginPage/logic.js
@@ -11,13 +11,25 @@ function myfunction() {
 function validate() {
   var password = document.getElementById("pass");
   var length = document.getElementById("length");
+  var email = document.getElementById("email");
 
-  if (password.value.length >= 5) {
-    alert("Login Succesfull");
+  if (password.value.match(" ")) {
+    alert(
+      "Login Failed!  please enter minimum 5 length password 1 special character 1 upper case and one lower case without giving space"
+    );
+  } else if (
+    password.value.length >= 5 &&
+    password.value.match(/[0-9]/i) &&
+    password.value.match(/[^A-Za-z0-9-' ']/i)
+  ) {
+    document.getElementById("check").style.color = "green";
+    alert("Login Succesfull Go to next Page");
     window.location.replace("newpage.html");
     return false;
   } else {
-    alert("Login Failed");
+    alert(
+      "Login Failed!  please enter minimum 5 length password 1 special character 1 upper case and one lower case without giving space"
+    );
   }
 }
 
